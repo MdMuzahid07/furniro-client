0145a1452507408d41a202e07de85addd40b5f73
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-05-07 18:23:19 +0600

    blog page, not completed

---
+++ src/app/(main)/blog/page.tsx
@@ -0,0 +1,13 @@
+import LocationBar from "@/components/shared/LocationBar";
+import SellingPointsModule from "@/components/shared/SellingPointsModule";
+
+const Blog = () => {
+  return (
+    <>
+      <LocationBar link="blog" title="Blog" />
+      <SellingPointsModule />
+    </>
+  );
+};
+
+export default Blog;
